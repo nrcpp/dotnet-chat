015ab418666c8ca2492e473361c4bb7d6b8a64c7
Denis K <den.kasakov@gmail.com>
2019-05-04 16:11:23 +0300

    Cleanup code. Layout improvements. Favicon.

--- SignalRChat/App_Start/RouteConfig.cs
+++ SignalRChat/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SignalRChat
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Chat", id = UrlParameter.Optional }
             );
         }
     }
--- SignalRChat/Controllers/HomeController.cs
+++ SignalRChat/Controllers/HomeController.cs
@@ -7,25 +7,7 @@ using System.Web.Mvc;
 namespace SignalRChat.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+    {   
         public ActionResult Chat()
         {
             return View();
